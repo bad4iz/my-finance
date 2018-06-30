ed6d52d7f1900da56a2d5051cbc82a569d28ac08
bad4iz <info@bad4iz.ru>
2018-06-30 17:52:04 +0400

    init

--- index.js
+++ index.js
@@ -14,25 +14,28 @@
 //     console.log(quotes)
 //
 // });
+var httpRequestOptions = {
+    proxy: 'http://localproxy.com'
+};
 
 
 var yahooFinance = require('yahoo-finance');
 
 yahooFinance.historical({
-    symbol: 'AAPL',
-    from: '2012-01-01',
-    to: '2012-12-31',
-    // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+    symbol: 'TATN.ME',
+    from: '2018-01-01',
+    to: '2018-12-31',
+    period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
 }, function (err, quotes) {
     //...
     console.table(quotes)
 });
 
-// This replaces the deprecated snapshot() API
-yahooFinance.quote({
-    symbol: 'AAPL',
-    modules: [ 'price', 'summaryDetail' ] // see the docs for the full list
-}, function (err, quotes) {
-    // ...
-    console.log(quotes)
-});
+// // This replaces the deprecated snapshot() API
+// yahooFinance.quote({
+//     symbol: 'AAPL',
+//     modules: [ 'price', 'summaryDetail' ] // see the docs for the full list
+// }, function (err, quotes) {
+//     // ...
+//     console.log(quotes)
+// });
